ecfc845b9c46d76ac5ffe9c66806e370dbe85678
neuefische <info@neuefische.de>
2023-09-29 16:13:59 +0200

    add CharacterInput.tsx component and remove CharactersOutput component

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
-import { useState } from 'react'
-import CharactersOutput from "./CharactersOutput.tsx";
-import {characters, RickAndMortyCharacter} from "./RickAndMortyCharacters.ts";
+import CharacterInput from "./CharacterInput.tsx"
+
 
 export default function App() {
 
-    const [character, setCharacter] = useState<RickAndMortyCharacter[]>(characters.results)
+
 
     return (
         <>
-            <CharactersOutput allCharacters={character}/>
+            <CharacterInput/>
         </>
     )
 }
